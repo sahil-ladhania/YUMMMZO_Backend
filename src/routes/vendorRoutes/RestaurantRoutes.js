83ba4cf782e29b0542d07bab3a3c8c556163630d
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2024-12-18 18:50:23 +0530

    Done Some Changes.

--- src/routes/vendorRoutes/RestaurantRoutes.js
+++ src/routes/vendorRoutes/RestaurantRoutes.js
@@ -4,8 +4,11 @@ import {authorize} from "../../middlewares/authN_authZ/Authorize.js";
 import { createRestaurant, deleteRestaurant, updateRestaurant } from '../../controllers/vendorControllers/restaurantManagement/RestaurantControllers.js';
 const router = express.Router();
 
-router.post('/restaurants' , authenticate , authorize('CREATE_RESTAURANT') , createRestaurant);
-router.put('/restaurants/:restaurantId' , authenticate , authorize('UPDATE_RESTAURANT') , updateRestaurant);
-router.delete('/restaurants/:restaurantId' , authenticate , authorize('DELETE_RESTAURANT') , deleteRestaurant);
+// router.post('/restaurants' , authenticate , authorize('CREATE_RESTAURANT') , createRestaurant);
+// router.put('/restaurants/:restaurantId' , authenticate , authorize('UPDATE_RESTAURANT') , updateRestaurant);
+// router.delete('/restaurants/:restaurantId' , authenticate , authorize('DELETE_RESTAURANT') , deleteRestaurant);
+router.post('/restaurants' , createRestaurant);
+router.put('/restaurants/:restaurantId' , updateRestaurant);
+router.delete('/restaurants/:restaurantId' , deleteRestaurant);
 
 export default router;
