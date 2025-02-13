3779f9a343919e599d3a48b2a6da63b20e1cd5f8
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2025-02-13 15:02:11 +0530

    Done Some Changes.

--- src/routes/vendorRoutes/OrderRoutes.js
+++ src/routes/vendorRoutes/OrderRoutes.js
@@ -13,7 +13,8 @@ const router = express.Router();
 
 router.get('/owner/:restaurantId/orders' , getAllActiveOrdersForARestaurant); // If Owner tries to get all Previous Orders -> getAllActiveOrdersForARestaurant will start executing.
 router.get('/owner/:restaurantId/orders/:orderId' , getAOrderForARestaurant); // If Owner tries to get an active Order -> getAOrderForARestaurant will start executing.
-router.put('/owner/:restaurantId/orders/accept-or-reject/:orderId' , validateRequest(acceptOrRejectOrderValidation) , acceptOrRejectOrder_R); // If Owner tries to update the status of Order -> acceptOrRejectOrder_R will start executing.
+// router.put('/owner/:restaurantId/orders/accept-or-reject/:orderId' , validateRequest(acceptOrRejectOrderValidation) , acceptOrRejectOrder_R); // If Owner tries to update the status of Order -> acceptOrRejectOrder_R will start executing.
+router.put('/owner/:restaurantId/orders/accept-or-reject/:orderId' , acceptOrRejectOrder_R); // If Owner tries to update the status of Order -> acceptOrRejectOrder_R will start executing.
 router.put('/owner/:restaurantId/orders/update-order-status-to-in-progress/:orderId' , validateRequest(updateOrderStatusToOrderInProgressValidation) , updateOrderStatusToInProgress_R); // If Owner tries to update the status of Order -> updateOrderStatusToInProgress_R will start executing.
 router.put('/owner/:restaurantId/orders/update-order-status-to-out-for-delivery/:orderId' , validateRequest(updateOrderStatusToOutForDeliveryValidation) , updateOrderStatusToOutForDelivery_R); // If Owner tries to update the status of Order -> updateOrderStatusToOutForDelivery_R will start executing.
 
